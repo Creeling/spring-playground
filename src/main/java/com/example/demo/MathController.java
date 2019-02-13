d140926f764c221b4c3211d78a23fac058496f46
White, Cree (PLV0136) <Cree.White@usaa.com>
2019-02-13 10:39:04 -0600

    wrote a few tests for controller and got them to pass for calculate endpoint

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,23 @@ public class MathController {
     @GetMapping("/pi")
     public String myMethod() {
         return "3.141592653589793";
+    }
+
+    @GetMapping("/calculate")
+    public String calculateMethod(@RequestParam String operation, @RequestParam String x, @RequestParam String y) {
+        String result = "";
+        if (operation.equals("add")) {
+            int addResult = Integer.parseInt(x) + Integer.parseInt(y);
+            result = x + " + " + y  + " = " + Integer.toString(addResult);
+        }
+        else if (operation.equals("subtract")) {
+            int subtractResult = Integer.parseInt(x) - Integer.parseInt(y);
+            result = x + " - " + y  + " = " + Integer.toString(subtractResult);
+        }
+        else if (operation.equals("multiply")) {
+            int multiplyResult = Integer.parseInt(x) * Integer.parseInt(y);
+            result = x + " * " + y  + " = " + Integer.toString(multiplyResult);
+        }
+        return result;
     }
 }
--- src/test/java/com/example/demo/MathControllerTest.java
+++ src/test/java/com/example/demo/MathControllerTest.java
@@ -21,10 +21,31 @@ public class MathControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void testEndpoint() throws Exception {
+    public void testPiEndpoint() throws Exception {
         this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
+    }
+
+    @Test
+    public void testCalculateEndpointWithAdd() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testCalculateEndpointWithSubtract() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testCalculateEndpointWithMultiply() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
     }
 
 }
